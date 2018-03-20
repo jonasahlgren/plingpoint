e33bb5b6dbe51a1c52032a3575cec54258673953
Jonas <jonasahlgren@gmail.com>
2018-03-20 10:06:13 +0100

    testing

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send(`Hello pingpoint is answering! ${req.baseUrl}`);
+    res.send(`Hello pingpoint is answering again! ${req.baseUrl}`);
 });
 
 app.listen(3000);
